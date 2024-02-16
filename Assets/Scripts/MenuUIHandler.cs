2ce837d02bc2d02f03e05fada4dd966e33f71b96
Dawn Law <dawnelaw@gmail.com>
2024-02-16 09:16:09 -0700

    added StartNew

--- Assets/Scripts/MenuUIHandler.cs
+++ Assets/Scripts/MenuUIHandler.cs
@@ -5,6 +5,8 @@ using UnityEngine.SceneManagement;
 
 public class MenuUIHandler : MonoBehaviour
 {
+    public string playerNameEntered;
+
     // Start is called before the first frame update
     void Start()
     {
